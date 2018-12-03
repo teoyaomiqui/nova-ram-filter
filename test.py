cfd912d96334f96136445488d8993992753dfdd4
Kostiantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2018-12-03 10:23:38 +0200

    renaming file with filterclass

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from nova.scheduler.host_manager import HostState
-import ram_filter
+import resource_filter
 from oslo_log import log
 from oslo_config import cfg
 import sys
@@ -13,6 +13,6 @@ CONF(sys.argv[1:])
 CONF.set_override('use_stderr', False)
 host_state = HostState("node-101", "node-77701.stc.bluvalt.com", "12lfaslk-3123fadsfsdjlk")
 
-source_driver = ram_filter.ActualResourceFilter()
+source_driver = resource_filter.ActualResourceFilter()
 
 print(source_driver.host_passes(host_state, "test"))
